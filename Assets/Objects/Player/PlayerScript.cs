9219cb3ad220776faa9c9ac1c56b005b64ab3795
Noah S <errorhomes@gmail.com>
2020-08-03 02:11:15 +0200

    Animation finished

--- Assets/Objects/Player/PlayerScript.cs
+++ Assets/Objects/Player/PlayerScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
     public float speed = 2f;
 
     void Start() {
@@ -11,10 +13,19 @@ public class PlayerScript : MonoBehaviour {
     }
 
     void Update() {
-        if(Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D)) {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+                animator.Play("walk");
             gameObject.transform.Translate(Time.deltaTime * speed, 0, 0);
+            spriteRenderer.flipX = false;
         } else if (Input.GetKey(KeyCode.A)) {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+                animator.Play("walk");
             gameObject.transform.Translate(-Time.deltaTime * speed, 0, 0);
+            spriteRenderer.flipX = true;
+        } else {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk"))
+                animator.Play("idle");
         }
     }
 
