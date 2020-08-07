61b954be57f324c6309150d09a0ec35a39e63b94
DaniBande <Daniel2.bandelow@t-online.de>
2020-08-07 14:38:34 +0200

    speaker

---
+++ Assets/Objects/labor/speaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speaker : MonoBehaviour
+{
+    bool triggered = false;
+    [SerializeField]
+    public bool neuTriggerbar;
+    [TextArea(3, 10)]
+    public string text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            GameObject.FindGameObjectWithTag("Heinz").GetComponent<TalkingHeinz>().readInfo(text);
+            triggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (neuTriggerbar && collision.gameObject.CompareTag("Player"))
+        {
+            triggered = false;
+        }
+    }
+}
